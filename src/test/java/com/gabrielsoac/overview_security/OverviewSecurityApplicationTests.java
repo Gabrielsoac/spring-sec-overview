4f382c2bda892b874d30f0414d64ab254ca42f56
Gabriel Soares <gabrielsoacc@gmail.com>
2026-04-26 10:38:48 -0300

    feat: created setup for spring boot project

---
+++ src/test/java/com/gabrielsoac/overview_security/OverviewSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielsoac.overview_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverviewSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
