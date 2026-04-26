0dfc95a36a66322ca992ba0156f370fab99bc3c4
Gabriel Soares <gabrielsoacc@gmail.com>
2026-04-26 10:55:06 -0300

    feat: craeted bootstrap application

---
+++ src/main/java/com/gabrielsoac/overview_security/OverviewSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.gabrielsoac.overview_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverviewSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverviewSecurityApplication.class, args);
+	}
+}
